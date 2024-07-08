b1c5b338b63717ac87e56e8d8295b285ca4a1e38
NelsonJfsg <nelin09001@gmail.com>
2024-07-07 17:35:12 -0700

    database test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ScheduleWeb';
+  user : any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  async ngOnInit() {
+    this.http.get('https://localhost:7082/Auth/get-all-users').subscribe((data) => {
+      this.user = data;
+      console.log(data);
+    });
+  }
+  
 }
